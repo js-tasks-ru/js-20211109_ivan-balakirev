457c233af844dc0a0d18bd938806e0fe5e041e4f
Ivan Balakirev <ivan.balakirev@gmail.com>
2021-11-14 13:25:58 +0300

    Added sort, pick, omit

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,33 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings( arr, param = 'asc' ) {
+  const unicodeArr = [];
+  const latinArr = [];
 
+  // Split array to unicode and lat to sort them separately
+  for ( let i = 0; i < arr.length; i ++ ) {
+    if ( arr[ i ].charCodeAt( 0 ) > 255 ) {
+      unicodeArr.push( arr[ i ] );
+    } else {
+      latinArr.push( arr[ i ] );
+    }
+  }
+
+  latinArr.sort( function( a, b ) {
+    return a.localeCompare( b, undefined, { caseFirst: 'upper' } );
+  } );
+
+  unicodeArr.sort( function( a, b ) {
+    return a.localeCompare( b, undefined, { caseFirst: 'upper' } );
+  } );
+
+  if ( param === 'desc' ) {
+    latinArr.reverse();
+    unicodeArr.reverse();
+    return latinArr.concat( unicodeArr );
+  }
+
+  return unicodeArr.concat( latinArr );
 }
+
