9715a9f6c228bfb7847b1fcbd251ce07fe11c5a8
Ivan Balakirev <ivan.balakirev@gmail.com>
2021-11-29 18:48:46 +0300

    Sort table v2 + tooltip

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,178 @@
 export default class SortableTable {
-  constructor(headersConfig, {
+  constructor( headersConfig, {
     data = [],
     sorted = {}
-  } = {}) {
+  } = {} ) {
+    this.headerConfig = headersConfig;
+    this.sorted = sorted;
 
+    // No need to seek for logic here
+    this.data = Array.isArray( data ) ? data : data.data;
+
+    this.render();
+  }
+
+  render() {
+    const elm = document.createElement( 'div' );
+    elm.innerHTML = this.template;
+    this.element = elm.firstElementChild;
+
+    document.body.append( elm );
+    this.subElements = this.getSubElements( this.element );
+
+    // Apply default sorting
+    if ( this.sorted.id ) {
+      this.sort( this.sorted.id, this.sorted.order ? this.sorted.order : null );
+    } else {
+      this.subElements.header.addEventListener( 'pointerdown', this.hightlightArrowEvents.bind( this ) );
+    }
+  }
+
+  hightlightArrowEvents( e ) {
+    const closestHeader = e.target.closest( '.sortable-table__cell' );
+    const isSortable = closestHeader.getAttribute( 'data-sortable' );
+
+    // Do nothing
+    if ( isSortable !== 'true' ) {
+      return;
+    }
+
+    const direction = closestHeader.getAttribute( 'data-order' );
+    const id = closestHeader.getAttribute( 'data-id' )
+
+    // Toggle direction and sort
+    this.sort( id, ( direction === 'desc' ) ? 'asc' : 'desc' );
+  }
+
+
+  highlightActiveArrow( id = this.sorted.id, order = this.sorted.order ) {
+    const $header = document.getElementsByClassName( 'sortable-table__header' )[ 0 ];
+    const $arrows = $header.querySelectorAll( '[data-sortable="true"]' );
+
+    for ( const arrow of $arrows ) {
+      if ( arrow.getAttribute( 'data-id' ) === id ) {
+        arrow.setAttribute( 'data-order', order )
+      } else {
+        arrow.setAttribute( 'data-order', '' );
+      }
+    }
+  }
+
+  remove() {
+    if ( this.element ) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove()
+    this.element = null;
+    this.subElements = {};
+  }
+
+  getSubElements( element ) {
+    return {
+      body: element.getElementsByClassName( 'sortable-table__body' )[ 0 ],
+      header: element.getElementsByClassName( 'sortable-table__header' )[ 0 ],
+    };
+  }
+
+  isSortable( type ) {
+    for ( const items of this.headerConfig ) {
+      if ( items.id === type ) {
+        return !! items.sortable;
+      }
+    }
+
+    return false;
+  }
+
+  sort( field = 'title', direction = 'desc' ) {
+    if ( ! this.isSortable( field ) ) {
+      return;
+    }
+
+    this.data = this.sortHelper( this.data, direction, field );
+    this.element.innerHTML = this.template;
+    this.subElements = this.getSubElements( this.element );
+
+    this.subElements.header.addEventListener( 'pointerdown', this.hightlightArrowEvents.bind( this ) );
+
+    this.highlightActiveArrow( field, direction );
+  }
+
+  sortHelper( arr, direction = 'asc', field ) {
+    // Make a copy not to change the original array
+    let newArr = [...arr];
+
+    return newArr.sort( function( a, b ) {
+      const first = direction === 'desc' ? b[ field ] : a[ field ];
+      const last = direction === 'desc' ? a[ field ] : b[ field ];
+
+      if ( typeof first === 'number' ) {
+        return first - last;
+      } else if ( typeof first === 'string' ) {
+        return first.localeCompare( last, ['ru', 'en'] );
+      }
+    } );
+  }
+
+  get template() {
+    return `<div data-element="productsContainer" class="products-list__container">
+                ${this.getHeaderTemplate()}
+              <div data-element="body" class="sortable-table__body">
+                ${this.getItemTemplate()}
+              </div>
+              <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+
+              <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+                <div>
+                  <p>No products satisfies your filter criteria</p>
+                  <button type="button" class="button-primary-outline">Reset all filters</button>
+                </div>
+              </div>
+            </div>`;
+  }
+
+  getHeaderTemplate() {
+    let $output = '<div data-element="header" class="sortable-table__header sortable-table__row">\n';
+    if ( this.headerConfig && this.headerConfig.length ) {
+      $output += this.headerConfig.map( function( data ) {
+
+        const $arrow = data.sortable
+          ? `<span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+             </span>`
+          : '';
+        return `<div class="sortable-table__cell" data-id="${data.id}" data-sortable="${data.sortable}" data-order="">
+                    <span>${data.title}</span>
+                    ${$arrow}
+                </div>\n`;
+      } ).join( '' );
+    }
+    $output += "\n</div>";
+
+    return $output;
+  }
+
+  getItemTemplate( data ) {
+    data = data ? data : this.data;
+
+    if ( data && data.length ) {
+      return this.data.map( function( item ) {
+        return `<a href="/products/${item.id}" class="sortable-table__row">${this.getCells( item, this.headerConfig )}</a>`;
+      }.bind( this ) ).join( '' );
+    }
+  }
+
+  getCells( item ) {
+    return this.headerConfig.map( function( config ) {
+      if ( config.template ) {
+        return config.template( item[ config.id ] );
+      } else {
+        return `<div class="sortable-table__cell">${item[ config.id ]}</div>`;
+      }
+    } ).join( '' );
   }
 }
+
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,59 @@
 class Tooltip {
+  constructor() {
+    // Create Singleton
+    if ( ! Tooltip.instance ) {
+      Tooltip.instance = this;
+    }
+    return Tooltip.instance;
+  }
 
+  initialize() {
+    document.addEventListener( 'pointerover', this.show.bind( this ) );
+    document.addEventListener( 'pointerout', this.hide.bind( this ) );
+    document.addEventListener( 'pointermove', this.getPosition.bind( this ) );
+  }
+
+  render( message ) {
+    const element = document.createElement( 'div' );
+    element.innerHTML = `<div class="tooltip">${message}</div>`;
+    this.element = element.firstElementChild;
+    document.body.append( this.element );
+  }
+
+  show( e ) {
+    if ( e.target.dataset.tooltip !== undefined ) {
+      this.render( e.target.dataset.tooltip );
+    }
+  }
+
+  hide() {
+    this.remove();
+  }
+
+  getPosition( e ) {
+    if ( e.target.dataset.tooltip !== undefined ) {
+      this.element.style.left = `${e.clientX}px`;
+      this.element.style.top = `${e.clientY + 10}px`;
+    }
+  }
+
+  removeListeners() {
+    document.removeEventListener( 'pointerover', this.show );
+    document.removeEventListener( 'pointerout', this.hide );
+    document.removeEventListener( 'pointerout', this.getPosition );
+  }
+
+  remove() {
+    if ( this.element ) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.removeListeners();
+    this.element = null;
+  }
 }
 
 export default Tooltip;
